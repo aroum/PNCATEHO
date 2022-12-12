7e0cdc45c1e9a5229e3d8df0e4b5f6b1364e7bc1
aroum <aroum24@gmail.com>
2022-12-12 22:34:26 +0300

    Delete firmware/QMK/PNCATEHO directory

--- firmware/QMK/PNCATEHO/keymaps/macropad/keymap.c
+++
@@ -1,8 +0,0 @@
-#include "PNCATEHO.h"
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [0] = LAYOUT(KC_B, KC_F, KC_T, KC_J,
-                 KC_H, KC_C, KC_N, KC_Y,
-                                   KC_X, KC_S),
-};
